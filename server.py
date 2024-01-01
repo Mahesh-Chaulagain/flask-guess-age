d728879e10b0cda227293de1e0375dd50f760f51
Mahesh-Chaulagain <maheshchaulagain8@gmail.com>
2024-01-01 10:36:55 +0545

    used jinja for multiline python statements

--- server.py
+++ server.py
@@ -2,7 +2,14 @@
 # -> a modern and designer-friendly templating language for python
 # -> specify which part be evaluated as python code inside the html file
 # -> when import is required in html file then it is best to write the code in python server
-# -> user double curly braces {{  }} in html file inorder to create single line of python expression
+# -> use double curly braces {{  }} in html file inorder to create single line of python expression
+# -> use curly braces with percentage sign {% %} for multiline python statement
+
+# Url Building
+# ->To build a URL to a specific function, use the url_for() function.
+# ->It accepts the name of the function as its first argument and any number of keyword arguments,
+#   each corresponding to a variable part of the URL rule.
+# -> Unknown variable parts are appended to the URL as query parameters.
 
 from flask import Flask, render_template
 import random, requests
@@ -38,6 +45,14 @@ def make_guess(name):
     age = age_data["age"]
 
     return render_template("guess.html", person_name=name, gender=gender, age=age , image=img_url)
+
+@app.route("/blog/<num>")
+def get_blog(num):
+    print(num)
+    blog_url = "https://api.npoint.io/f8b9cce0347af5bc4402"
+    response = requests.get(blog_url)
+    all_posts = response.json()
+    return render_template("blog.html", posts=all_posts)
 
 if __name__ == "__main__":
     app.run(debug=True)
